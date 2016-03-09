60a0aff7ce182aa229517ebdd2a8da286dccaf9e
freeman <the.freeman.lab@gmail.com>
2016-03-08 20:32:32 -0500

    expand tests

--- test.js
+++ test.js
@@ -3,24 +3,44 @@ var input = require('./index')
 
 test('construction', function (t) {
   var panel = input([
-    {type: 'range', label: 'range slider', min: 0, max: 100, initial: 20}
+    {type: 'range', label: 'range label', min: 0, max: 100, initial: 20}
   ])
-  t.equal(typeof document.querySelector('.input-panel'), 'object')
+  t.equal(typeof document.querySelector('#input-panel'), 'object')
   t.end()
 })
 
 test('range', function (t) {
   var panel = input([
-    {type: 'range', label: 'range slider', min: 0, max: 100, initial: 20}
+    {type: 'range', label: 'label', min: 0, max: 100, initial: 20}
   ])
   t.equal(typeof document.querySelector('.input-panel-range'), 'object')
+  t.equal(typeof document.querySelector('#input-panel-label'), 'object')
   t.end()
 })
 
 test('color', function (t) {
   var panel = input([
-    {type: 'color', label: 'range slider', min: 0, max: 100, initial: 20}
+    {type: 'color', label: 'label', min: 0, max: 100, initial: 20}
   ])
-  t.equal(typeof document.querySelector('.input-panel-range'), 'object')
+  t.equal(typeof document.querySelector('.input-panel-color'), 'object')
+  t.equal(typeof document.querySelector('#input-panel-label'), 'object')
+  t.end()
+})
+
+test('text', function (t) {
+  var panel = input([
+    {type: 'text', label: 'label', min: 0, max: 100, initial: 20}
+  ])
+  t.equal(typeof document.querySelector('.input-panel-text'), 'object')
+  t.equal(typeof document.querySelector('#input-panel-label'), 'object')
+  t.end()
+})
+
+test('checkbox', function (t) {
+  var panel = input([
+    {type: 'checkbox', label: 'label', min: 0, max: 100, initial: 20}
+  ])
+  t.equal(typeof document.querySelector('.input-panel-checkbox'), 'object')
+  t.equal(typeof document.querySelector('#input-panel-label'), 'object')
   t.end()
 })
